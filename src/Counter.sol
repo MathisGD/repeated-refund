edf2a1b95f89699a7d60e43b37434bcb9f595e8a
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-11-10 19:14:32 +0100

    chore: initial commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,42 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    error No();
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    uint256 private number = 2;
+
+    function set() public {
+        number = 1;
     }
 
-    function increment() public {
-        number++;
+    function setAndRevert() public {
+        number = 0;
+        revert No();
+    }
+}
+
+contract CounterTest {
+    Counter private counter;
+
+    uint256 a;
+
+    function setUp() public {
+        counter = new Counter();
+    }
+
+    function test() public {
+        assembly {
+            mstore(0xfff, 5)
+        }
+
+        Counter counterStack = counter;
+
+        counterStack.set();
+
+        uint256 i = 100;
+
+        while (i-- >= 2) {
+            try counterStack.setAndRevert() {} catch {}
+        }
     }
 }
